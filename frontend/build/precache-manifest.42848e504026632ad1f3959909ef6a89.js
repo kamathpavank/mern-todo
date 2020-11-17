8633695c80f474bd7d6307c6c9e60d435ee15205
Pavankumar.Kamath <pavankumar.kamath@ril.com>
2020-11-17 17:51:37 +0530

    build changes

---
+++ frontend/build/precache-manifest.42848e504026632ad1f3959909ef6a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78de91a779634cae6e32475fa555cbee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5251273709b6f3064c1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "598b357d95b6835df83b",
+    "url": "/static/css/main.5231334a.chunk.css"
+  },
+  {
+    "revision": "e5251273709b6f3064c1",
+    "url": "/static/js/2.2aceb1e8.chunk.js"
+  },
+  {
+    "revision": "598b357d95b6835df83b",
+    "url": "/static/js/main.57f925e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
